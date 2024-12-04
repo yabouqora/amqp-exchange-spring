e8118e0af6e02087f522578eecbd46786127ddec
Y.Abouqora <yabouqora@gmail.com>
2024-12-04 11:32:34 +0100

    first commit

---
+++ src/test/java/com/example/amqpexchangespring/AmqpExchangeSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.amqpexchangespring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AmqpExchangeSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
